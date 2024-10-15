970bb93a0ed56b806108b0dfedd1051b9ee2d61d
Pavel Kazlou <na9ort.poland@gmail.com>
2024-10-15 08:48:45 +0200

    [longest-substring-without-repeating-characters]: first version of implementation;

--- longest-substring-without-repeating-characters/src/main/java/org/techideas/leetcode/substring/Solution.java
+++ longest-substring-without-repeating-characters/src/main/java/org/techideas/leetcode/substring/Solution.java
@@ -1,7 +1,30 @@
 package org.techideas.leetcode.substring;
 
+import java.util.HashSet;
+
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        var chars = s.toCharArray();
+        var theLongestLength = 0;
+        var theLongestString = "";
+
+        for (int outerLoopIndex = 0; outerLoopIndex < chars.length; outerLoopIndex++) {
+            var set = new HashSet<Character>();
+            set.add(chars[outerLoopIndex]);
+
+            for (int innerLoopIndex = outerLoopIndex + 1; innerLoopIndex < chars.length; innerLoopIndex++) {
+                if (set.contains(chars[innerLoopIndex]) && set.size() > theLongestLength) {
+                    theLongestLength = set.size();
+                    theLongestString = set.toString();
+                    break;
+                }
+
+                set.add(chars[innerLoopIndex]);
+            }
+        }
+
+//        System.out.println("theLongestString = " + theLongestString);
+
+        return theLongestLength;
     }
 }
--- longest-substring-without-repeating-characters/src/test/java/org/techideas/leetcode/substring/SolutionTest.java
+++ longest-substring-without-repeating-characters/src/test/java/org/techideas/leetcode/substring/SolutionTest.java
@@ -25,15 +25,16 @@ public class SolutionTest {
 
     private static Stream<Arguments> providerForShouldReturnTheLongestSubstring() {
         return Stream.of(
-                //Explanation: The answer is "abc", with the length of 3.
-                Arguments.of("abcabcbb", 3),
-
-                //Explanation: The answer is "b", with the length of 1.
-                Arguments.of("bbbbb", 1),
-
-                //Explanation: The answer is "wke", with the length of 3.
-                //Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-                Arguments.of("pwwkew", 3)
+//                //Explanation: The answer is "abc", with the length of 3.
+//                Arguments.of("abcabcbb", 3),
+//
+//                //Explanation: The answer is "b", with the length of 1.
+//                Arguments.of("bbbbb", 1),
+//
+//                //Explanation: The answer is "wke", with the length of 3.
+//                //Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//                Arguments.of("pwwkew", 3),
+                Arguments.of(" ", 1)
         );
     }
 }
