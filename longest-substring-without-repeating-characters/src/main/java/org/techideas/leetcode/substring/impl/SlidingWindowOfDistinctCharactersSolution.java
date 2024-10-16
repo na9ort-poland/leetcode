846db67291b23676a5465f4396835c603d9b2c6c
Pavel Kazlou <na9ort.poland@gmail.com>
2024-10-16 19:54:00 +0200

    [longest-substring-without-repeating-characters]: added a part of sliding windows solution;

--- longest-substring-without-repeating-characters/src/main/java/org/techideas/leetcode/substring/impl/SlidingWindowOfDistinctCharactersSolution.java
+++ longest-substring-without-repeating-characters/src/main/java/org/techideas/leetcode/substring/impl/SlidingWindowOfDistinctCharactersSolution.java
@@ -2,16 +2,33 @@ package org.techideas.leetcode.substring.impl;
 
 import org.techideas.leetcode.substring.Solution;
 
+import java.util.HashSet;
+
 public class SlidingWindowOfDistinctCharactersSolution implements Solution {
 
     @Override
     public int lengthOfLongestSubstring(String s) {
-//        if (s.length() < 2) {
-//            return s.length();
-//        }
+        char[] chars = s.toCharArray();
+        var startIndex = 0;
+        var endIndex = 1;
+        var lengthOfTheLongestSubstring = 0;
+        var set = new HashSet<Character>();
 
+        set.add(chars[startIndex]);
 
+        while (endIndex < chars.length) {
+            System.out.printf("=====\nstartIndex = %d\nendIndex = %d\n=====%n", startIndex, endIndex);
+            if (set.contains(chars[endIndex])) {
+                ++startIndex;
+                set.remove(chars[endIndex]);
+                continue;
+            }
 
-        return 0;
+            lengthOfTheLongestSubstring = Math.max(endIndex - startIndex, lengthOfTheLongestSubstring);
+            set.add(chars[endIndex]);
+            ++endIndex;
+        }
+
+        return lengthOfTheLongestSubstring + 1;
     }
 }
