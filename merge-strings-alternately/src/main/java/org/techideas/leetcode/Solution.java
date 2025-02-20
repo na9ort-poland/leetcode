8ce75564d1966430915d654379d5eaec61bb129c
Pavel Kazlou <na9ort.poland@gmail.com>
2025-02-20 20:19:34 +0100

    [merge-strings-alternately]: done;

--- merge-strings-alternately/src/main/java/org/techideas/leetcode/Solution.java
+++ merge-strings-alternately/src/main/java/org/techideas/leetcode/Solution.java
@@ -2,6 +2,21 @@ package org.techideas.leetcode;
 
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        return "";
+        var word1Char = word1.toCharArray();
+        var word2Char = word2.toCharArray();
+        var length = Math.max(word1.length(), word2.length());
+
+        var builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            if (i < word1Char.length) {
+                builder.append(word1Char[i]);
+            }
+
+            if (i < word2Char.length) {
+                builder.append(word2Char[i]);
+            }
+        }
+
+        return builder.toString();
     }
 }
