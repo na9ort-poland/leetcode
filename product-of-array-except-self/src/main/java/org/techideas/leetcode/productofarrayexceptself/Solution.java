5370fe3a86869099e7d0d295b26edb99e6c0b0ee
Pavel Kazlou <na9ort.poland@gmail.com>
2025-03-01 10:16:41 +0100

    [product-of-array-except-self]: TDD approach: added tests;

---
+++ product-of-array-except-self/src/main/java/org/techideas/leetcode/productofarrayexceptself/Solution.java
@@ -0,0 +1,7 @@
+package org.techideas.leetcode.productofarrayexceptself;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        return new int[]{1};
+    }
+}
--- product-of-array-except-self/src/test/java/org/techideas/leetcode/productofarrayexceptself/SolutionTest.java
+++ product-of-array-except-self/src/test/java/org/techideas/leetcode/productofarrayexceptself/SolutionTest.java
@@ -1,7 +1,35 @@
 package org.techideas.leetcode.productofarrayexceptself;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SolutionTest {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    @Test
+    void example_1() {
+        //given
+        var nums = new int[]{1, 2, 3, 4};
+        var solution = new Solution();
+        var expectedResult = new int[]{24, 12, 8, 6};
+
+        //when
+        var result = solution.productExceptSelf(nums);
+
+        //then
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    void example_2() {
+        //given
+        var nums = new int[]{-1, 1, 0, -3, 3};
+        var solution = new Solution();
+        var expectedResult = new int[]{0, 0, 9, 0, 0};
+
+        //when
+        var result = solution.productExceptSelf(nums);
+
+        //then
+        Assertions.assertArrayEquals(expectedResult, result);
     }
 }
