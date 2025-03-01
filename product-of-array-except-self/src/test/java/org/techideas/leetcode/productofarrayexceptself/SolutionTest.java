806d65a165613407a021675228a90813e6cfa603
Pavel Kazlou <na9ort.poland@gmail.com>
2025-03-01 10:12:24 +0100

    [product-of-array-except-self]: created the project;

---
+++ product-of-array-except-self/src/test/java/org/techideas/leetcode/productofarrayexceptself/SolutionTest.java
@@ -0,0 +1,7 @@
+package org.techideas.leetcode.productofarrayexceptself;
+
+public class SolutionTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
